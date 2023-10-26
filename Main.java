d5af6d331d230d740c564be58407c3e96f1db038
Harshit Saxena <74541424+HarshitSaxena12@users.noreply.github.com>
2023-10-27 03:30:37 +0530

    1sst Commit n

---
+++ Main.java
@@ -0,0 +1,21 @@
+package LinkedList.java;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+
+
+        list.insertFirst(1);
+        list.insertFirst(3);
+        list.insertFirst(5);
+
+        list.display();
+
+
+
+
+
+    }
+}
